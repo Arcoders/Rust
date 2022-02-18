dffd25d11b5213e1aacc9750a1172d89f0c3a485
arcoders <anonismaa@gmail.com>
2022-02-18 19:44:27 +0100

    HashMap examples

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 fn main() {
     get_element_in_position(2);
     get_element_in_position(100);
     map_vector();
     vector_variants();
+    hash_maps();
+    split_words();
 }
 
 fn get_element_in_position(position: usize) {
@@ -41,4 +45,31 @@ fn vector_variants() {
         SpreadsheetCell::Int(i) => println!("{}", i),
         _=> println!("Not a integer!")
     };
+}
+
+fn hash_maps() {
+    let blue = String::from("Blue");
+    let yellow = String::from("Yellow");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(blue, 10);
+    scores.insert(yellow, 50);
+    scores.entry(String::from("Red")).or_insert(30);
+
+    for (key, value) in &scores {
+        println!("{} {}", key, value);
+    }
+} 
+
+fn split_words() {
+    let text = "Lorem ipsum dolor sit amet";
+    let mut map = HashMap::new();
+
+    for word in text.split_ascii_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
