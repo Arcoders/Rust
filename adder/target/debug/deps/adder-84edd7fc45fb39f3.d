d71d2e0ef2917e5a4f3fcc8be1790a7c8ab4f08c
arcoders <anonismaa@gmail.com>
2022-02-22 22:00:09 +0100

    minigrep cli app

---
+++ adder/target/debug/deps/adder-84edd7fc45fb39f3.d
@@ -0,0 +1,5 @@
+c:\Users\kaula\Desktop\Rust\adder\target\debug\deps\adder-84edd7fc45fb39f3.rmeta: src\lib.rs
+
+c:\Users\kaula\Desktop\Rust\adder\target\debug\deps\adder-84edd7fc45fb39f3.d: src\lib.rs
+
+src\lib.rs:
